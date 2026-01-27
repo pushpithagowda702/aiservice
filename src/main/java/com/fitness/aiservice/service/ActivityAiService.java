910c1f5c3a6001fe9f3decefab185160e860d7cc
Pushpitha <pushpithagowda776@gmail.com>
2026-01-27 15:47:34 +0530

    import update

--- src/main/java/com/fitness/aiservice/service/ActivityAiService.java
+++ src/main/java/com/fitness/aiservice/service/ActivityAiService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
